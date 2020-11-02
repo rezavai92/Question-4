2127d7ec8f59b4e2731968dc35680c16b887f3fe
rezavai <nijhumreza52@gmail.com>
2020-11-02 07:25:58 -0800

    initial

---
+++ que4.js
@@ -0,0 +1,23 @@
+function wearPPE() {
+    return new Promise((resolve) => {
+        setTimeout(() => resolve('Wearing PPE'), 2000);
+    })
+}
+
+function fightCorona() {
+    return new Promise((resolve) => {
+
+       
+      setTimeout(()=>{
+               
+        resolve("fighting corona")},1000)  
+
+    } )
+}
+function execute() {
+    wearPPE().then(result => console.log(result));
+    fightCorona().then(result => setTimeout(()=>{console.log(result)},1000) );
+}
+
+execute();
+
